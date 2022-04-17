ce2be13fb813f70cfff886069f855396aac06beb
Julio Canizalez <juliocanizalez@outlook.com>
2022-04-16 23:20:15 -0600

    [ADD] initial imports

---
+++ view/imports.php
@@ -0,0 +1,18 @@
+<!-- BOOTSTRAP RELATED -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<!-- @CUSTOM STYLES -->
+<link rel="stylesheet" href="/web/css/styles.css">
+
+<!-- SCROLLREVEAL IMPORT -->
+<script defer src="https://unpkg.com/scrollreveal"></script>
+
+<!-- JQUERY IMPORT -->
+<script async src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<!-- SWEET ALERT 2 IMPORT -->
+<script defer src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- @CUSTOM SCRIPTS -->
+<script defer src="/web/js/main.js"></script>
