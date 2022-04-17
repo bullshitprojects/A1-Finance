76b0d4dfaf28a258ce81bda540bd7bed20884bba
Julio Canizalez <juliocanizalez@outlook.com>
2022-04-17 00:52:12 -0600

    [ADD] initial conetent to home module

---
+++ view/home.php
@@ -0,0 +1,44 @@
+<?php
+/*session_start();
+if (!$_SESSION['validate']) {
+    header('location:index.php?path=login');
+    exit();
+}*/
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include 'imports.php';
+    ?>
+    <title>Dashboard | A1 Finance</title>
+</head>
+
+<body>
+    <main class="container-fluid main">
+        <div class="row">
+            <!-- LEFT MENU -->
+            <div class="col-2 menu">
+                <?php
+                include 'modules/logo.php'
+                ?>
+                <div class="border-gradient"></div>
+            </div>
+
+            <!-- MAIN CONTENT HERE -->
+            <div class="col-8 border-both main-content">
+                <!-- CONTENIDO DEL CENTRO -->
+            </div>
+
+            <!-- LEFT SECTION -->
+            <div class="col-2 left-section"></div>
+        </div>
+    </main>
+</body>
+
+</html>
