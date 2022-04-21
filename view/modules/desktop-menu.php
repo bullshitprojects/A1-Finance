2d84f82dbf0769f66ae2da360dc067621ab7d2de
Julio Canizalez <juliocanizalez@outlook.com>
2022-04-21 01:26:27 -0600

    [ADD] desktop menu

---
+++ view/modules/desktop-menu.php
@@ -0,0 +1,11 @@
+<div class="col-2 menu">
+    <?php
+    include 'logo.php'
+    ?>
+    <div class="border-gradient"></div>
+    <div class="menu-container">
+        <a href="/" class="dashboard-icon">Tablero</a>
+        <a href="/?path=profile" class="profile-icon">Perfil</a>
+        <a href="/?path=settings" class="settings-icon">Ajustes</a>
+    </div>
+</div>
