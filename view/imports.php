8aa7044bd0877d0bfdfd4fbdecaf10c4a124b314
Julio Canizalez <juliocanizalez@outlook.com>
2022-04-20 21:55:07 -0600

    [UPDATE] imports url

--- view/imports.php
+++ view/imports.php
@@ -3,7 +3,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 <!-- @CUSTOM STYLES -->
-<link rel="stylesheet" href="/web/css/styles.css">
+<link rel="stylesheet" href="../web/css/styles.css">
 
 <!-- SCROLLREVEAL IMPORT -->
 <script defer src="https://unpkg.com/scrollreveal"></script>
@@ -15,4 +15,4 @@
 <script defer src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <!-- @CUSTOM SCRIPTS -->
-<script defer src="/web/js/main.js"></script>
+<script defer src="../web/js/main.js"></script>
